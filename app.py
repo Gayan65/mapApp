88c197cbe2df6d8446d2b42817f65d99f981943f
Gayan Withana Gamage <gayan65728@yahoo.com>
2024-02-23 14:31:37 +0200

    app.js method and index.html methord make to POST

--- app.py
+++ app.py
@@ -1,15 +1,18 @@
 from flask import Flask, render_template, request
+from apis.api import get_direction
 
 app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 
 def index():
-    if request.method == 'GET':
+    if request.method == 'POST':
         # Access form data from query parameters (GET method)
-        origin = request.args.get('origin')
-        destination = request.args.get('destination')
+        origin = request.form.get('origin')
+        destination = request.form.get('destination')
         print(origin, destination)
+        get_direction(origin, destination)
+
     return render_template('index.html')
 
 if __name__ == "__main__":
