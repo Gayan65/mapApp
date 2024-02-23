88a72609d35104df8f4a116a6ab9d74bb469ec78
Gayan Withana Gamage <gayan65728@yahoo.com>
2024-02-23 12:58:58 +0200

    logs the route details

--- apis/api.py
+++ apis/api.py
@@ -1,8 +1,28 @@
+import urllib.parse
 import requests
 
 # Get request 
-response = requests.get('https://www.mapquestapi.com/directions/v2/route?key=47WpsLDQHmWJWawsQQdjNujjszN7xLee&from=Clarendon%20Blvd,Arlington,VA&to=2400+S+Glebe+Rd,+Arlington,+VA')
+# response = requests.get('https://www.mapquestapi.com/directions/v2/route?key=47WpsLDQHmWJWawsQQdjNujjszN7xLee&from=Clarendon%20Blvd,Arlington,VA&to=2400+S+Glebe+Rd,+Arlington,+VA')
 
-print(response.status_code)
+main_api = "https://www.mapquestapi.com/directions/v2/route?"
+orig = "Washington"
+dest = "Baltimaore"
+key = "47WpsLDQHmWJWawsQQdjNujjszN7xLee"
 
-print(response.json())
+url = main_api + urllib.parse.urlencode({"key": key, "from":orig, "to":dest})
+
+print("URL: " + (url))
+
+json_data = requests.get(url).json()
+json_status = json_data["info"]["statuscode"]
+
+if json_status == 0:
+    print("API Status: " + str(json_status) + " = A successful route call.\n")
+    print("Directions from " + (orig) + " to " + (dest))
+    print("Trip Duration:   " + (json_data["route"]["formattedTime"]))
+    print("Kilometers:      " + str("{:.2f}".format((json_data["route"]["distance"])*1.61)))
+
+    print("=============================================")
+    for each in json_data["route"]["legs"][0]["maneuvers"]:
+        print((each["narrative"]) + " (" + str("{:.2f}".format((each["distance"])*1.61) + " km)"))
+    print("=============================================\n")
