f81ef99afe60954a91209dffbe1bb810ee3e8142
Gayan Withana Gamage <gayan65728@yahoo.com>
2024-02-26 11:05:03 +0200

    add dotenv

--- apis/api.py
+++ apis/api.py
@@ -1,10 +1,14 @@
+import os
 import urllib.parse
 import requests
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Api Call for direction
 def get_direction(orig, dest):
     main_api = "https://www.mapquestapi.com/directions/v2/route?"
-    key = "47WpsLDQHmWJWawsQQdjNujjszN7xLee"
+    key = os.getenv("KEY")
     url = main_api + urllib.parse.urlencode({"key": key, "from":orig, "to":dest})
     json_data = requests.get(url).json()
     return json_data
